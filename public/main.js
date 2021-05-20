cd679a63122a6741cb82c6dcc4d357433114522b
Gabriel Willian <obielwb@gmail.com>
2021-05-20 19:35:57 -0300

    uptade main

--- public/main.js
+++ public/main.js
@@ -2,25 +2,8 @@ function ExibirNumero(caracter) {
     document.getElementById("resultado").value += caracter;
 }
 
-// var valorInput = [];
 function ExibirOperacao(caracter) {
     document.getElementById("resultado").value += caracter;
-    /*
-    var input = document.getElementById("resultado").value;
-    console.log(input);
-    for (var i in input) {
-        valorInput = [];
-        valorInput.push(i);
-    }
-    console.log(valorInput);
-    var ultimoValor = valorInput[valorInput.length - 1];
-    if (ultimoValor == '+' || ultimoValor == '-' || ultimoValor == '*' || ultimoValor == '/' ) {
-        alert("Operação Inválida!");
-        input = " ";
-    }
-    else 
-        document.getElementById("resultado").value += caracter;
-    */
 }
 
 function Resetar() {
