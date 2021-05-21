125d897401f5e0816cb086983b4ff0a3d87d2070
Gabriel Willian <obielwb@gmail.com>
2021-05-21 09:44:54 -0300

    uptade main.js

--- public/main.js
+++ public/main.js
@@ -1,16 +1,16 @@
-function ExibirNumero(caracter) {
+function exibirNumero(caracter) {
     document.getElementById("resultado").value += caracter;
 }
 
-function ExibirOperacao(caracter) {
+function exibirOperacao(caracter) {
     document.getElementById("resultado").value += caracter;
 }
 
-function Resetar() {
+function resetar() {
     document.getElementById("resultado").value = " ";
 }
 
-function CalcularResultado() {
+function calcularResultado() {
     let conta = document.querySelector("#resultado").value;
     if (conta)
         document.querySelector("#resultado").value = eval(conta);
